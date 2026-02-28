d349cba27a8f02c7de370d79dcb185ea76fe3100
hide0621 <fujihara_hideyuki@retail-ai.jp>
2026-02-28 11:10:35 +0900

    feat: A*B + A*C を整理して、A * (B + C) の状態を表すコードを実装

--- lib/sample/sample6/sample6.dart
+++ lib/sample/sample6/sample6.dart
@@ -1,5 +1,25 @@
 // A*B + A*C の状態を表すコード
 
+// class Order {
+//   bool isPremiumMember;
+//   int cartTotal;
+//   bool hasCoupon;
+
+//   Order(this.isPremiumMember, this.cartTotal, this.hasCoupon);
+
+//   bool isFreeShipping() {
+//     // A*B + A*C の状態
+//     if ((isPremiumMember && cartTotal >= 2000) ||
+//         (isPremiumMember && hasCoupon)) {
+//       return true;
+//     }
+//     return false;
+//   }
+// }
+
+// リファクタリング前のコード
+// A*B + A*C を整理して、A * (B + C) の状態を表すコード
+
 class Order {
   bool isPremiumMember;
   int cartTotal;
@@ -8,11 +28,29 @@ class Order {
   Order(this.isPremiumMember, this.cartTotal, this.hasCoupon);
 
   bool isFreeShipping() {
-    // A*B + A*C の状態
-    if ((isPremiumMember && cartTotal >= 2000) ||
-        (isPremiumMember && hasCoupon)) {
-      return true;
-    }
-    return false;
+    // 算術演算の論理で整理: A * (B + C)
+    return isPremiumMember && (cartTotal >= 2000 || hasCoupon);
   }
 }
+
+// リファクタリング後のコード
+// A*B + A*C を整理して、A * (B + C) の状態を表すコード
+
+// class Order {
+//   bool isPremiumMember;
+//   int cartTotal;
+//   bool hasCoupon;
+
+//   Order(this.isPremiumMember, this.cartTotal, this.hasCoupon);
+
+//   bool isFreeShipping() {
+//     // 算術演算の論理で整理: A * (B + C)
+//     // 整理されたことで「isEligibleForDiscount」という関数の抽出が明確に！
+//     return isPremiumMember && _isEligibleForDiscount();
+//   }
+
+//   // 抽出した関数
+//   bool _isEligibleForDiscount() {
+//     return cartTotal >= 2000 || hasCoupon;
+//   }
+// }
