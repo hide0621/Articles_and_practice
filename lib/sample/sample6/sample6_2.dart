b65a11eb747f6edf47eb0c591475ec025cd0b518
hide0621 <fujihara_hideyuki@retail-ai.jp>
2026-02-28 11:55:12 +0900

    feat: ド・モルガンの法則を適用したコード

--- lib/sample/sample6/sample6_2.dart
+++ lib/sample/sample6/sample6_2.dart
@@ -1,5 +1,25 @@
 // !A && !Bの状態
 
+// class Article {
+//   bool isDraft;
+//   bool isDeleted;
+//   String content;
+
+//   Article(this.isDraft, this.isDeleted, this.content);
+
+//   void publish() {
+//     // !A && !B の状態。否定が連続していて直感的でない
+//     if (!isDraft && !isDeleted) {
+//       print('記事を公開: $content');
+//     } else {
+//       print('公開できません');
+//     }
+//   }
+// }
+
+// !A && !B の状態を整理して、!(A || B) の状態を表すコード
+// ド・モルガンの法則を適用
+
 class Article {
   bool isDraft;
   bool isDeleted;
@@ -8,11 +28,17 @@ class Article {
   Article(this.isDraft, this.isDeleted, this.content);
 
   void publish() {
-    // !A && !B の状態。否定が連続していて直感的でない
-    if (!isDraft && !isDeleted) {
+    // !(A || B) の状態。
+    // 「公開不可な状態（isUnpublishable）」の【否定】として読めるようになる
+    if (!_isUnpublishable()) {
       print('記事を公開: $content');
     } else {
       print('公開できません');
     }
+  }
+
+  // 抽出した関数：肯定的な条件（OR）にまとまったことでロジックが明快に
+  bool _isUnpublishable() {
+    return isDraft || isDeleted;
   }
 }
