17fb0a38aef80e2372558abb7983cb47d530b5ed
hide0621 <fujihara_hideyuki@retail-ai.jp>
2026-02-28 11:14:51 +0900

    refactor: A*B + A*C を整理して、A * (B + C) の状態を表すコードを活かしてリファクタリング

--- lib/sample/sample6/sample6.dart
+++ lib/sample/sample6/sample6.dart
@@ -20,22 +20,6 @@
 // リファクタリング前のコード
 // A*B + A*C を整理して、A * (B + C) の状態を表すコード
 
-class Order {
-  bool isPremiumMember;
-  int cartTotal;
-  bool hasCoupon;
-
-  Order(this.isPremiumMember, this.cartTotal, this.hasCoupon);
-
-  bool isFreeShipping() {
-    // 算術演算の論理で整理: A * (B + C)
-    return isPremiumMember && (cartTotal >= 2000 || hasCoupon);
-  }
-}
-
-// リファクタリング後のコード
-// A*B + A*C を整理して、A * (B + C) の状態を表すコード
-
 // class Order {
 //   bool isPremiumMember;
 //   int cartTotal;
@@ -45,12 +29,28 @@ class Order {
 
 //   bool isFreeShipping() {
 //     // 算術演算の論理で整理: A * (B + C)
-//     // 整理されたことで「isEligibleForDiscount」という関数の抽出が明確に！
-//     return isPremiumMember && _isEligibleForDiscount();
-//   }
-
-//   // 抽出した関数
-//   bool _isEligibleForDiscount() {
-//     return cartTotal >= 2000 || hasCoupon;
+//     return isPremiumMember && (cartTotal >= 2000 || hasCoupon);
 //   }
 // }
+
+// リファクタリング後のコード
+// A*B + A*C を整理して、A * (B + C) の状態を表すコード
+
+class Order {
+  bool isPremiumMember;
+  int cartTotal;
+  bool hasCoupon;
+
+  Order(this.isPremiumMember, this.cartTotal, this.hasCoupon);
+
+  bool isFreeShipping() {
+    // 算術演算の論理で整理: A * (B + C)
+    // 整理されたことで「isEligibleForDiscount」という関数の抽出が明確に！
+    return isPremiumMember && _isEligibleForDiscount();
+  }
+
+  // 抽出した関数
+  bool _isEligibleForDiscount() {
+    return cartTotal >= 2000 || hasCoupon;
+  }
+}
