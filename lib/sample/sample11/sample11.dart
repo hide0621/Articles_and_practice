08244ad2a09ec62d0f2297d5d46f12acfec72880
fujihara hideyuki <96705805+hide0621@users.noreply.github.com>
2026-06-13 12:48:07 +0900

    refactor: 共通結合をコンストラクタインジェクションで解消

--- lib/sample/sample11/sample11.dart
+++ lib/sample/sample11/sample11.dart
@@ -5,16 +5,38 @@ class Repository {
   }
 }
 
-/// グローバルに参照可能なシングルトン
-final repository = Repository();
+// /// グローバルに参照可能なシングルトン
+// final repository = Repository();
+
+// class UseCase1 {
+//   Future<String> execute() async {
+//     return await repository.fetchData();
+//   }
+// }
+
+// class UseCase2 {
+//   Future<String> execute() async {
+//     return await repository.fetchData();
+//   }
+// }
+
+/// コンストラクタインジェクションを使用して依存関係を注入
 
 class UseCase1 {
+  final Repository repository;
+
+  UseCase1(this.repository);
+
   Future<String> execute() async {
     return await repository.fetchData();
   }
 }
 
 class UseCase2 {
+  final Repository repository;
+
+  UseCase2(this.repository);
+
   Future<String> execute() async {
     return await repository.fetchData();
   }
