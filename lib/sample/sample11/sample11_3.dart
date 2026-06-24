fe5c2c9f86bc7a4cfe14b61d5ef87fecf551ca53
fujihara hideyuki <96705805+hide0621@users.noreply.github.com>
2026-06-24 19:21:21 +0900

    refactor: 外部結合を引数と戻り値を用いて解消

--- lib/sample/sample11/sample11_3.dart
+++ lib/sample/sample11/sample11_3.dart
@@ -1,19 +1,39 @@
+// // ignore: dangling_library_doc_comments
+// /// 外部結合に設計上のアンチパターン
+
+// // ignore: avoid_init_to_null
+// int? parameter = null;
+
+// // ignore: avoid_init_to_null
+// int? result = null;
+
+// class Calculator {
+//    int calculate() {
+//     if (parameter == null) {
+//       throw Exception('parameter is null');
+//     }
+//     result = parameter! * 2;
+//     return result!;
+//   }
+// }
+
+// class Caller {
+//   final Calculator calculator;
+
+//   Caller(this.calculator);
+
+//   void callCalculator() {
+//     parameter = 5; 
+//     print(calculator.calculate());
+//   }
+// }
+
 // ignore: dangling_library_doc_comments
-/// 外部結合に設計上のアンチパターン
-
-// ignore: avoid_init_to_null
-int? parameter = null;
-
-// ignore: avoid_init_to_null
-int? result = null;
+/// 外部結合を引数と戻り値を用いて解消した例
 
 class Calculator {
-   int calculate() {
-    if (parameter == null) {
-      throw Exception('parameter is null');
-    }
-    result = parameter! * 2;
-    return result!;
+  int calculate(int parameter) {
+    return parameter * 2;
   }
 }
 
@@ -23,7 +43,7 @@ class Caller {
   Caller(this.calculator);
 
   void callCalculator() {
-    parameter = 5; 
-    print(calculator.calculate());
+    int parameter = 5; 
+    print(calculator.calculate(parameter));
   }
 }
