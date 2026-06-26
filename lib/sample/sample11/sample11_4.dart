a1f71dd18ff04c4ff28faee147ca024108b3db38
fujihara hideyuki <96705805+hide0621@users.noreply.github.com>
2026-06-26 19:04:40 +0900

    refactor: 制御結合を「操作対象による分割」でリファクタリング

--- lib/sample/sample11/sample11_4.dart
+++ lib/sample/sample11/sample11_4.dart
@@ -12,3 +12,16 @@
 //     iconView.image = CHECK_ICON;
 //   }
 // }
+
+// ignore: dangling_library_doc_comments
+/// 制御結合を「操作対象による分割」でリファクタリングした例
+
+// void updateView(bool isError) {
+//   resultView.isVisible = !isError;
+//   errorView.isVisible = isError;
+//   iconView.image = getIconImage(isError);
+// }
+
+// Image getIconImage(bool isError) {
+//   return isError ? CROSS_ICON : CHECK_ICON;
+// }
