4add665e890c15c3bcbe7258d0933d14f51ae13e
fujihara hideyuki <96705805+hide0621@users.noreply.github.com>
2026-06-26 18:54:50 +0900

    feat: 制御結合のサンプルコードを実装

---
+++ lib/sample/sample11/sample11_4.dart
@@ -0,0 +1,14 @@
+// ignore: dangling_library_doc_comments
+/// 制御結合の例
+
+// void updateView(bool isError) {
+//   if (isError) {
+//     resultView.isVisible = true;
+//     errorView.isVisible = false;
+//     iconView.image = CROSS_ICON;
+//   } else {
+//     resultView.isVisible = false;
+//     errorView.isVisible = true;
+//     iconView.image = CHECK_ICON;
+//   }
+// }
