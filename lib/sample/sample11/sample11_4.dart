8650adbd486ad8127ff94f4865b52647c57c4c43
fujihara hideyuki <96705805+hide0621@users.noreply.github.com>
2026-06-27 17:35:26 +0900

    feat: 制御結合の別の例と解消法を書いた

--- lib/sample/sample11/sample11_4.dart
+++ lib/sample/sample11/sample11_4.dart
@@ -25,3 +25,84 @@
 // Image getIconImage(bool isError) {
 //   return isError ? CROSS_ICON : CHECK_ICON;
 // }
+
+// ignore: dangling_library_doc_comments
+/// 制御結合の例
+
+// class ProfileViewPresenter {
+//   void updateUserView(DateType dateType) {
+//     switch (dateType) {
+//       case DateType.userName:
+//         var userName = getUserName(dateType.userId);
+//         userNameView.text = userName;
+//         break;
+//       case DateType.birthDate:
+//         var birthDate = formatBirthDate(dateType.birthDate);
+//         birthDateView.text = birthDate;
+//         break;
+//       case DateType.ProfileImage:
+//         var profileImageBitmap = getProfileImageBitmap(dateType.userId);
+//         profileImageView.image = profileImageBitmap;
+//         break;
+//     }
+//   }
+// }
+
+// ignore: dangling_library_doc_comments
+/// 上記、制御結合が引き起こす呼び出し元のパターンの例
+// class Caller {
+//   void callUpateUserView() {
+//     switch (dateType) {
+//       case satisfiesCondition:
+//         var dateType = DateType.userName;
+//         profileViewPresenter.updateUserView(dateType);
+//         break;
+//       case satisfiesAnotherCondition:
+//         var dateType = DateType.birthDate;
+//         profileViewPresenter.updateUserView(dateType);
+//         break;
+//       case satisfiesYetAnotherCondition:
+//         var dateType = DateType.ProfileImage;
+//         profileViewPresenter.updateUserView(dateType);
+//         break;
+//     }
+//   }
+// }
+
+// ignore: dangling_library_doc_comments
+/// 条件分岐内の処理に関連性がない制御結合を関心ごとに分割した例
+
+// class ProfileViewPresenter {
+//   void updateUserView() {
+//     var userName = getUserName();
+//     userNameView.text = userName;
+//   }
+
+//   void updateBirthDateView() {
+//     var birthDate = formatBirthDate();
+//     birthDateView.text = birthDate;
+//   }
+
+//   void updateProfileImageView() {
+//     var profileImageBitmap = getProfileImageBitmap();
+//     profileImageView.image = profileImageBitmap;
+//   }
+// }
+
+// class Caller1 {
+//   void someMethod() {
+//     profileViewPresenter.updateUserView();
+//   }
+// }
+
+// class Caller2 {
+//   void someMethod() {
+//     profileViewPresenter.updateBirthDateView();
+//   }
+// }
+
+// class Caller3 {
+//   void someMethod() {
+//     profileViewPresenter.updateProfileImageView();
+//   }
+// }
