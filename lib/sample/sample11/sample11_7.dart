8fcaec58bb27fe83c7629a275965fa807a60161c
fujihara hideyuki <96705805+hide0621@users.noreply.github.com>
2026-07-03 19:10:30 +0900

    feat: メッセージ結合の例を実装

---
+++ lib/sample/sample11/sample11_7.dart
@@ -0,0 +1,10 @@
+// ignore: dangling_library_doc_comments
+/// メッセージ結合の例
+// class Caller {
+//   final Closable closable;
+//   Caller(this.closable);
+
+//   void doSomething() {
+//     closable.close();
+//   }
+// }
