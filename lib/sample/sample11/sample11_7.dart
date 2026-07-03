ad9a2d3e1fd3842fdb56f0b3395e1702c7cfbcc4
fujihara hideyuki <96705805+hide0621@users.noreply.github.com>
2026-07-03 19:35:42 +0900

    refactor: 内容結合になっているメッセージ結合をリファクタリング

--- lib/sample/sample11/sample11_7.dart
+++ lib/sample/sample11/sample11_7.dart
@@ -9,14 +9,30 @@
 //   }
 // }
 
+// ignore: dangling_library_doc_comments
+/// 内容結合を含むメッセージ結合の例
+
 // class Caller {
 //   final UserListPresenter _userListPresenter;
 //   Caller({UserListPresenter userListPresenter}) : _userListPresenter = userListPresenter;
 
 //   void updateUserList() {
 //     final users = ...
-//     userListPresenter.users = users;
+//     _userListPresenter.users = users;
 //     ...
-//     userListPresenter.notifyUserListUpdated();
+//     _userListPresenter.notifyUserListUpdated();
+//   }
+// }
+
+// ignore: dangling_library_doc_comments
+/// リファクタリング後
+
+// class Caller {
+//   final UserListPresenter _userListPresenter;
+//   Caller({UserListPresenter userListPresenter}) : _userListPresenter = userListPresenter;
+
+//   void updateUserList() {
+//     final users = ...
+//     _userListPresenter.notifyUserListUpdated(users);
 //   }
 // }
