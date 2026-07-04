63bd982c9121875323afe45e21d88d5e43cbb04b
fujihara hideyuki <96705805+hide0621@users.noreply.github.com>
2026-07-04 12:21:36 +0900

    feat: 表面的にはメッセージ結合だが、実質はデータ結合になっていて、斜め読みもしにくい例を実装

--- lib/sample/sample11/sample11_7.dart
+++ lib/sample/sample11/sample11_7.dart
@@ -36,3 +36,33 @@
 //     _userListPresenter.notifyUserListUpdated(users);
 //   }
 // }
+
+// ignore: dangling_library_doc_comments
+/// 表面的にはメッセージ結合だが、実質はデータ結合になっていて、斜め読みもしにくい例
+
+// class Caller {
+//   final ErrorViewPresenter _errorViewPresenter;
+//   Caller({ErrorViewPresenter errorViewPresenter}) : _errorViewPresenter = errorViewPresenter;
+
+//   void updateViews() {
+//     var result = ...;
+//     ...
+//     var isErrorViewVisible = result.isError;
+//     if (isErrorViewVisible) {
+//       _errorViewPresenter.showErrorView();
+//     } else {
+//       _errorViewPresenter.hideErrorView();
+//     }
+//   }
+// }
+
+// class ErrorViewPresenter {
+//   ...
+//   void showErrorView() {
+//     view.isVisible = true;
+//   }
+
+//   void hideErrorView() {
+//     view.isVisible = false;
+//   }
+// }
