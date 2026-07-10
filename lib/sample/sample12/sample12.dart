edfb70bba7c1c1cd9b817ca44d177e47d1593711
fujihara hideyuki <96705805+hide0621@users.noreply.github.com>
2026-07-10 13:54:19 +0900

    feat: 初期化mainを実装

--- lib/sample/sample12/sample12.dart
+++ lib/sample/sample12/sample12.dart
@@ -8,3 +8,21 @@ class GiftPoint {
     }
   }
 }
+
+void main() {
+  try {
+    final giftPoint = GiftPoint(-5);
+  } catch (e) {
+    // ignore: avoid_print
+    print(e); // Output: GiftPoint value cannot be less than 0
+  }
+
+  final standardMemberShipPoint = GiftPoint(3000);
+  // ignore: avoid_print
+  print(standardMemberShipPoint.value); // Output: 10
+
+  final premiumMemberShipPoint = GiftPoint(5000);
+
+  // ignore: avoid_print
+  print(premiumMemberShipPoint.value); // Output: 10
+}
